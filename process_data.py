7bdee6e28d8ddb82219c5e7294a2a118e3f3b9c3
Omer Shubi <omer.shubi@gmail.com>
2021-06-04 13:24:30 +0300

    - merge preprocessing functions - add tweet-preprocessing to requirments (not currently used)

--- process_data.py
+++ process_data.py
@@ -5,7 +5,7 @@ import csv
 import matplotlib.pyplot as plt
 import re
 from sklearn.model_selection import train_test_split
-
+import preprocessor as p
 def analyze_stance_data():
     df['Target'].value_counts().plot.bar(rot=45)
     plt.savefig('counts_per_domain.png', bbox_inches='tight')
@@ -38,33 +38,41 @@ def open_blitzer_data():
     with open(src_path + "test", 'rb') as f:
         test = pickle.load(f)
 
-    pass
 
-def preprocess_unlabeled_stance_data():
-    df = pd.read_csv('StanceDataset/domain_tweets_all.txt', sep='\t', header=None)
-    df.columns = ['tweet_ID', 'date', 'tweet', 'hashtags', 'Target']
-    df_head = df.head(100)
 
-    def filter_hashtags(tweet: str, hashtags: str):
-        """
-        Remove all hashtags that were used in the filtering collection data.
-        Remove urls
-        :param tweet:
-        :param hashtags:
-        :return:
-        """
-        for hashtag in hashtags.split():
-            tweet = tweet.replace("#" + hashtag, "")
-        tweet = re.sub(r"http\S+", "", tweet)
-        return tweet
+def filter_hashtags(tweet: str, hashtags: str):
+    """
+    Remove all hashtags that were used in the filtering collection data.
+    Remove urls
+    :param tweet:
+    :param hashtags:
+    :return:
+    """
+    for hashtag in hashtags.split():
+        tweet = tweet.replace("#" + hashtag, "")
+    tweet = re.sub(r"http\S+", "", tweet) # TODO change per decision
+    return tweet
 
-    # remove query hashtags and urls from tweets
-    df['clean_tweet'] = df.apply(lambda row: filter_hashtags(row['tweet'], row['hashtags']), axis=1)
+def preprocess_stance_data(data_path, is_labeled):
+    if is_labeled:
+        df = pd.read_excel(data_path)
+        df = df.drop('Opinion Towards', axis=1)
 
-    df['clean_tweet'].replace({'#': '', "@": ''}, inplace=True, regex=True)
+        df['Sentiment'].replace({'pos': 1,
+                                 'neg': 0,
+                                 'other': None}, inplace=True)
+        df.dropna(inplace=True)
+        df['Sentiment'] = df['Sentiment'].astype(int)
 
-    # Remove tweets with multiple classes
-    df = df[df["Target"].str.contains(",") == False]
+    else:
+        df = pd.read_csv('StanceDataset/domain_tweets_all.txt', sep='\t', header=None)
+        df.columns = ['tweet_ID', 'date', 'Tweet', 'hashtags', 'Target']
+        # remove query hashtags and urls from tweets
+
+        df['Tweet'] = df.apply(lambda row: filter_hashtags(row['Tweet'], row['hashtags']), axis=1)
+        # Remove tweets with multiple classes
+        df = df[df["Target"].str.contains(",") == False]
+
     df['Target'].replace({'Hillary Clinton': 'hillary',
                           'Feminist Movement': 'feminist',
                           'Legalization of Abortion': 'abortion',
@@ -72,54 +80,31 @@ def preprocess_unlabeled_stance_data():
                           'Climate Change is a Real Concern': 'climate',
                           'Atheism': 'atheism'}, inplace=True)
 
+    # TODO choose clean method
+    # df['Tweet'].replace({'#': '', "@": ''}, inplace=True, regex=True)
+
+    # Save files seperately per class
     for _class in ['hillary', 'feminist', 'abortion', 'trump',
                    'climate', 'atheism']:
-        class_df = df.loc[df["Target"] == _class, 'clean_tweet']
-        x = class_df.tolist()
         save_path = os.path.join('data', _class)
-        print(f"Saving {len(x)} unlabeled {_class} tweets to {save_path}")
         os.makedirs(save_path, exist_ok=True)
-        with open(os.path.join(save_path, 'unlabeled'), 'wb') as f:
-            pickle.dump(x, f)
-
-    exit()
-
-def preprocess_labeled_stance_data():
-    df = pd.read_excel("StanceDataset/full_data.xlsx").drop('Opinion Towards', axis=1)
-
-    df['Target'].replace({'Hillary Clinton': 'hillary',
-                          'Feminist Movement': 'feminist',
-                          'Legalization of Abortion': 'abortion',
-                          'Donald Trump': 'trump',
-                          'Climate Change is a Real Concern': 'climate',
-                          'Atheism': 'atheism'}, inplace=True)
-
-    df['Sentiment'].replace({'pos': 1,
-                          'neg': 0,
-                          'other': None}, inplace=True)
-    df.dropna(inplace=True)
-    df['Sentiment'] = df['Sentiment'].astype(int)
-
-    df['Tweet'].replace({'#': '', "@": ''}, inplace=True, regex=True)
-
-    for _class in ['hillary', 'feminist', 'abortion', 'trump',
-                   'climate', 'atheism']:
         x = df.loc[df["Target"] == _class, 'Tweet'].tolist()
 
-        y = df.loc[df["Target"] == _class, 'Sentiment'].tolist()
-        save_path = os.path.join('data', _class)
-        print(f"Saving {len(x)} labeled {_class} tweets to {save_path}")
-        os.makedirs(save_path, exist_ok=True)
-
-        with open(os.path.join(save_path, 'test'), 'wb') as f:
-            pickle.dump((x, y), f)
-
-        x_train, x_dev, y_train, y_dev = train_test_split(x,y, test_size=0.15, stratify=y, random_state=42)
-        with open(os.path.join(save_path, 'train'), 'wb') as f:
-            pickle.dump((x_train, y_train), f)
-        with open(os.path.join(save_path, 'dev'), 'wb') as f:
-            pickle.dump((x_dev, y_dev), f)
+        print(f"Saving {len(x)} is_labeled={is_labeled} {_class} tweets to {save_path}")
+        if is_labeled:
+            y = df.loc[df["Target"] == _class, 'Sentiment'].tolist()
+            with open(os.path.join(save_path, 'test'), 'wb') as f:
+                pickle.dump((x, y), f)
+            x_train, x_dev, y_train, y_dev = train_test_split(x, y, test_size=0.15, stratify=y, random_state=42)
+            with open(os.path.join(save_path, 'train'), 'wb') as f:
+                pickle.dump((x_train, y_train), f)
+            with open(os.path.join(save_path, 'dev'), 'wb') as f:
+                pickle.dump((x_dev, y_dev), f)
+        else:
+            with open(os.path.join(save_path, 'unlabeled'), 'wb') as f:
+                pickle.dump(x, f)
 
 if __name__ == '__main__':
-    preprocess_labeled_stance_data()
-    preprocess_unlabeled_stance_data()
+    preprocess_stance_data("StanceDataset/full_data.xlsx", is_labeled=True)
+    preprocess_stance_data('StanceDataset/domain_tweets_all.txt', is_labeled=False)
+
